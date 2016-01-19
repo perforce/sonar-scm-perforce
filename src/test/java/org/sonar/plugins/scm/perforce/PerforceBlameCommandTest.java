b88e6ea8c77a1effd33326a4fa41ec67cdd21b87
Tyrel Haveman <tyrel@binarypeople.net>
2016-01-19 10:23:20 -0800

    Use p4 filelog and p4 annotate first, then get changelist
    In order to try to optimize the blame process for a file in Perforce,
    first run filelog which usually returns all revisions, then run
    annotate. If we then encounter a changelist we don't know, attempt to
    look it up. If that fails, too, fail gracefully.

--- src/test/java/org/sonar/plugins/scm/perforce/PerforceBlameCommandTest.java
+++ src/test/java/org/sonar/plugins/scm/perforce/PerforceBlameCommandTest.java
@@ -20,25 +20,23 @@
 package org.sonar.plugins.scm.perforce;
 
 import com.perforce.p4java.core.IChangelist;
+import com.perforce.p4java.core.file.FileSpecOpStatus;
 import com.perforce.p4java.core.file.IFileAnnotation;
+import com.perforce.p4java.core.file.IFileRevisionData;
+import com.perforce.p4java.core.file.IFileSpec;
 import com.perforce.p4java.option.server.GetFileAnnotationsOptions;
+import com.perforce.p4java.option.server.GetRevisionHistoryOptions;
 import com.perforce.p4java.server.IOptionsServer;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
 import org.junit.Test;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.scm.BlameCommand.BlameOutput;
 import org.sonar.api.batch.scm.BlameLine;
 
+import java.util.*;
+
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyList;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.*;
 
 public class PerforceBlameCommandTest {
 
@@ -51,7 +49,7 @@ public class PerforceBlameCommandTest {
     IFileAnnotation annotation = mock(IFileAnnotation.class);
     when(annotation.getDepotPath()).thenReturn(null);
 
-    when(server.getFileAnnotations(anyList(), any(GetFileAnnotationsOptions.class))).thenReturn(Collections.singletonList(annotation));
+    when(server.getFileAnnotations(anyListOf(IFileSpec.class), any(GetFileAnnotationsOptions.class))).thenReturn(Collections.singletonList(annotation));
 
     command.blame(mock(InputFile.class), server, blameOutput);
 
@@ -64,24 +62,51 @@ public class PerforceBlameCommandTest {
     IOptionsServer server = mock(IOptionsServer.class);
     PerforceBlameCommand command = new PerforceBlameCommand(mock(PerforceConfiguration.class));
 
-    IFileAnnotation annotation = mock(IFileAnnotation.class);
-    when(annotation.getDepotPath()).thenReturn("foo/bar/src/Foo.java");
-    when(annotation.getLower()).thenReturn(3);
+    IFileAnnotation annotation1 = mock(IFileAnnotation.class);
+    when(annotation1.getDepotPath()).thenReturn("foo/bar/src/Foo.java");
+    when(annotation1.getLower()).thenReturn(3);
 
-    when(server.getFileAnnotations(anyList(), any(GetFileAnnotationsOptions.class))).thenReturn(Arrays.asList(annotation, annotation));
+    IFileAnnotation annotation2 = mock(IFileAnnotation.class);
+    when(annotation2.getDepotPath()).thenReturn("foo/bar/src/Foo.java");
+    when(annotation2.getLower()).thenReturn(3);
+
+    IFileAnnotation annotation3 = mock(IFileAnnotation.class);
+    when(annotation3.getDepotPath()).thenReturn("foo/bar/src/Foo.java");
+    when(annotation3.getLower()).thenReturn(4);
+
+    IFileAnnotation annotation4 = mock(IFileAnnotation.class);
+    when(annotation4.getDepotPath()).thenReturn("foo/bar/src/Foo.java");
+    when(annotation4.getLower()).thenReturn(5);
+
+    Map<IFileSpec, List<IFileRevisionData>> result = new HashMap<>();
+    IFileSpec fileSpecResult = mock(IFileSpec.class);
+    when(fileSpecResult.getOpStatus()).thenReturn(FileSpecOpStatus.VALID);
+    IFileRevisionData revision3 = mock(IFileRevisionData.class);
+    when(revision3.getChangelistId()).thenReturn(3);
+    Date date = new Date();
+    when(revision3.getDate()).thenReturn(date);
+    when(revision3.getUserName()).thenReturn("jhenry");
+    result.put(fileSpecResult, Collections.singletonList(revision3));
+
+    when(server.getRevisionHistory(anyListOf(IFileSpec.class), any(GetRevisionHistoryOptions.class))).thenReturn(result);
+    when(server.getFileAnnotations(anyListOf(IFileSpec.class), any(GetFileAnnotationsOptions.class))).thenReturn(Arrays.asList(annotation1, annotation2, annotation3, annotation4));
 
     IChangelist changelist = mock(IChangelist.class);
-    Date date = new Date();
     when(changelist.getDate()).thenReturn(date);
-    when(changelist.getUsername()).thenReturn("jhenry");
-    when(server.getChangelist(3)).thenReturn(changelist);
+    when(changelist.getUsername()).thenReturn("bgates");
+    when(server.getChangelist(4)).thenReturn(changelist);
+
+    when(server.getChangelist(5)).thenReturn(null);
 
     InputFile inputFile = mock(InputFile.class);
     command.blame(inputFile, server, blameOutput);
 
-    BlameLine line = new BlameLine().revision("3").date(date).author("jhenry");
-    verify(blameOutput).blameResult(inputFile, Arrays.asList(line, line));
-    verify(server, times(1)).getChangelist(3);
+    BlameLine line1 = new BlameLine().revision("3").date(date).author("jhenry");
+    BlameLine line2 = new BlameLine().revision("3").date(date).author("jhenry");
+    BlameLine line3 = new BlameLine().revision("4").date(date).author("bgates");
+    BlameLine line4 = new BlameLine().revision("5").date(new Date(0)).author("unknown");
+    verify(blameOutput).blameResult(inputFile, Arrays.asList(line1, line2, line3, line4));
+    verify(server, times(1)).getChangelist(4);
   }
 
   @Test
@@ -94,7 +119,7 @@ public class PerforceBlameCommandTest {
     when(annotation.getDepotPath()).thenReturn("foo/bar/src/Foo.java");
     when(annotation.getLower()).thenReturn(3);
 
-    when(server.getFileAnnotations(anyList(), any(GetFileAnnotationsOptions.class))).thenReturn(Collections.singletonList(annotation));
+    when(server.getFileAnnotations(anyListOf(IFileSpec.class), any(GetFileAnnotationsOptions.class))).thenReturn(Collections.singletonList(annotation));
 
     IChangelist changelist = mock(IChangelist.class);
     Date date = new Date();
