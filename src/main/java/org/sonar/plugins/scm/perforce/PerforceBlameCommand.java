e0f2552552ab53bafca450c95fce3cc4f856852e
Tyrel Haveman <tyrel@binarypeople.net>
2016-01-19 10:23:20 -0800

    Use p4 filelog and p4 annotate first, then get changelist
    In order to try to optimize the blame process for a file in Perforce,
    first run filelog which usually returns all revisions, then run
    annotate. If we then encounter a changelist we don't know, attempt to
    look it up. If that fails, too, fail gracefully.

--- src/main/java/org/sonar/plugins/scm/perforce/PerforceBlameCommand.java
+++ src/main/java/org/sonar/plugins/scm/perforce/PerforceBlameCommand.java
@@ -21,17 +21,17 @@ package org.sonar.plugins.scm.perforce;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.perforce.p4java.core.IChangelist;
+import com.perforce.p4java.core.file.FileSpecOpStatus;
 import com.perforce.p4java.core.file.IFileAnnotation;
+import com.perforce.p4java.core.file.IFileRevisionData;
 import com.perforce.p4java.core.file.IFileSpec;
 import com.perforce.p4java.exception.P4JavaException;
 import com.perforce.p4java.impl.generic.core.file.FileSpec;
 import com.perforce.p4java.option.server.GetFileAnnotationsOptions;
+import com.perforce.p4java.option.server.GetRevisionHistoryOptions;
 import com.perforce.p4java.server.IOptionsServer;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,6 +44,7 @@ public class PerforceBlameCommand extends BlameCommand {
 
   private static final Logger LOG = LoggerFactory.getLogger(PerforceBlameCommand.class);
   private final PerforceConfiguration config;
+  private final Map<Integer, IFileRevisionData> revisionDataMap = new HashMap<>();
   private final Map<Integer, IChangelist> changelistMap = new HashMap<>();
 
   public PerforceBlameCommand(PerforceConfiguration config) {
@@ -78,21 +79,61 @@ public class PerforceBlameCommand extends BlameCommand {
       return;
     }
 
+    // Get history of file
+    Map<IFileSpec, List<IFileRevisionData>> revisionMap = server.getRevisionHistory(fileSpecs, getRevisionHistoryOptions());
+    for (Map.Entry<IFileSpec, List<IFileRevisionData>> entry : revisionMap.entrySet()) {
+      IFileSpec revisionFileSpec = entry.getKey();
+      if (!FileSpecOpStatus.VALID.equals(revisionFileSpec.getOpStatus()) && !FileSpecOpStatus.INFO.equals(revisionFileSpec.getOpStatus())) {
+        String statusMessage = fileSpec.getStatusMessage();
+        LOG.debug("Unable to get revisions of file " + inputFile + " [" + statusMessage + "]. Skipping it.");
+        return;
+      }
+      for (IFileRevisionData revisionData : entry.getValue()) {
+        revisionDataMap.put(revisionData.getChangelistId(), revisionData);
+      }
+    }
+
     // Compute blame, getting changelist from server if not already retrieved
     List<BlameLine> lines = new ArrayList<>();
     for (IFileAnnotation fileAnnotation : fileAnnotations) {
       int lowerChangelistId = fileAnnotation.getLower();
 
-      IChangelist changelist = changelistMap.get(lowerChangelistId);
-      if (changelist == null) {
-        changelist = server.getChangelist(lowerChangelistId);
-        changelistMap.put(lowerChangelistId, changelist);
+      IFileRevisionData data = revisionDataMap.get(lowerChangelistId);
+      if (data != null) {
+
+        lines.add(new BlameLine()
+            .revision(String.valueOf(lowerChangelistId))
+            .date(data.getDate())
+            .author(data.getUserName()));
+
+      } else {
+        // Sometimes they're missing from the revision history, so try to get it directly.
+
+        LOG.debug("Changelist " + lowerChangelistId + " was not found in history for " + inputFile + ". It will be fetched directly.");
+        IChangelist changelist = changelistMap.get(lowerChangelistId);
+        if (changelist == null) {
+          changelist = server.getChangelist(lowerChangelistId);
+          if (changelist != null) { // sometimes even that can fail due to cross-server imports
+            changelistMap.put(lowerChangelistId, changelist);
+          }
+        }
+
+        if (changelist != null) {
+          lines.add(new BlameLine()
+              .revision(String.valueOf(lowerChangelistId))
+              .date(changelist.getDate())
+              .author(changelist.getUsername()));
+        } else {
+          // We really couldn't get any information for this changelist!
+          // Unfortunately, blame information is required for every line...
+          lines.add(new BlameLine()
+              .revision(String.valueOf(lowerChangelistId))
+              .date(new Date(0))
+              .author("unknown"));
+        }
+
       }
 
-      lines.add(new BlameLine()
-        .revision(String.valueOf(lowerChangelistId))
-        .date(changelist.getDate())
-        .author(changelist.getUsername()));
     }
 
     // SONARPLUGINS-3097: Perforce does not report blame on last empty line, so populate from last line with blame
@@ -111,8 +152,22 @@ public class PerforceBlameCommand extends BlameCommand {
   private static GetFileAnnotationsOptions getFileAnnotationOptions() {
     GetFileAnnotationsOptions options = new GetFileAnnotationsOptions();
     options.setUseChangeNumbers(true);
-    options.setFollowAllIntegrations(true);
+    options.setFollowBranches(true);
     options.setIgnoreWhitespaceChanges(true);
+    return options;
+  }
+
+  /**
+   * Creating options for revision history command (filelog).
+   *
+   * @return options for requests.
+   */
+  private static GetRevisionHistoryOptions getRevisionHistoryOptions() {
+    GetRevisionHistoryOptions options = new GetRevisionHistoryOptions();
+    options.setIncludeInherited(true);
+    options.setLongOutput(true);
+    options.setMaxRevs(1000);
+    options.setOmitNonContributaryIntegrations(true);
     return options;
   }
 
